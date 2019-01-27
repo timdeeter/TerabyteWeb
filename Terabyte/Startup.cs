1307a36a5b189058bfa9b04e5c674ed772d4155c
timdeeter <timdeeter1@gmail.com>
2019-01-27 02:47:38 -0500

    Add project files.

---
+++ Terabyte/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Terabyte.Startup))]
+namespace Terabyte
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
